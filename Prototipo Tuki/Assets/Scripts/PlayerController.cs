578026b5f4af76ad00db3bec54d07b69cbe37a16
JuanAngarita20 <80299132+JuanAngarita20@users.noreply.github.com>
2023-11-15 02:07:54 -0500

    Cambios Fisicas 15

--- Prototipo Tuki/Assets/Scripts/PlayerController.cs
+++ Prototipo Tuki/Assets/Scripts/PlayerController.cs
@@ -1,8 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.Assertions.Must;
+using UnityEngine.UI;
 using UnityEngineInternal;
 
 
@@ -28,13 +30,18 @@ public class PlayerController : MonoBehaviour
 
     public Vector3 movement; 
     private bool checkWallOnFront = false;
+
+
+    //Safe rotation
+     
   
 
     // Start is called before the first frame update
     void Start()
     {
         estado = State.normal;
-        dir_mov = Mov_Dir.right;
+        dir_mov =Mov_Dir.right;
+        movement = new Vector3(0.0f, 0.0f ,0.0f);
     
     }
 
@@ -46,25 +53,47 @@ public class PlayerController : MonoBehaviour
         //Verificar input para girar
         if(Input.GetKey("right") ){
             
-            characterRigidBody.transform.rotation = Quaternion.Euler(0, 0, 0);
+            /*characterRigidBody.transform.rotation = Quaternion.Euler(0, 0, 0);*/
             
+            if(dir_mov == Mov_Dir.left){
+                //gameObject.transform.rotation =  Quaternion.AngleAxis(180.0f,Vector3.up)*gameObject.transform.rotation; -> TECNICAMENTE DEBERIA SERVIR, SUPONGO QUE NO
+                // POR USAR transformp,rotate cuando hay fisicas
+            
+                characterRigidBody.MoveRotation(Quaternion.AngleAxis(180.0f,Vector3.up)*characterRigidBody.rotation);
+                //characterRigidBody.rotation = Quaternion.Euler(0, 180, 0)*characterRigidBody.rotation;
+                Debug.Log("Girar derecha");
+            }
+
             dir_mov = Mov_Dir.right;
+            movement = new Vector3(1.0f, 0.0f ,0.0f);
+            
 
            
         }
         
         if(Input.GetKey("left")){
             
-            characterRigidBody.rotation = Quaternion.Euler(0, 180, 0);
-            
+            /*characterRigidBody.transform.rotation = Quaternion.Euler(0, 180, 0);*/
+            //gameObject.transform.rotation =  Quaternion.Euler(0, 180, 0);
+
+            if(dir_mov == Mov_Dir.right){
+                
+                characterRigidBody.MoveRotation(Quaternion.AngleAxis(180.0f,Vector3.up)*characterRigidBody.rotation);
+                //characterRigidBody.rotation = Quaternion.Euler(0, 180, 0)*characterRigidBody.rotation;
+                Debug.Log("Girar izq");
+            }
+
+
             dir_mov = Mov_Dir.left;
+            movement = new Vector3(-1.0f, 0.0f ,0.0f);
+            
         }
         
-        movement = new Vector3(Input.GetAxis("Horizontal"), 0.0f ,0.0f);
-        //moveCharacter(movement);
+        //movement = new Vector3(Input.GetAxis("Horizontal"), 0.0f ,0.0f);
+        
 
         
-        if(Input.GetKeyDown("space") && estado == State.normal && jumpMax < 1 ){
+        if(Input.GetKeyDown("space") && jumpMax < 1 ){
 
             jumpMax++;
     
@@ -92,44 +121,62 @@ public class PlayerController : MonoBehaviour
        moveCharacter(movement); // We call the function 'moveCharacter' in FixedUpdate for Physics movement
 
        
-      
-       
     }
 
 
     void moveCharacter(Vector3 direction)
     {
+        //int verificarNumero;
 
+        //Movimiento con Fisicas correctas - Mas pesado
         if(dir_mov == Mov_Dir.left){ //Para moverse correctamente cuando se gira
             if(!checkWallOnFront){
-                Vector3 moveVector = transform.TransformDirection(direction)*-speed;
+                Vector3 moveVector = direction*speed;
                 characterRigidBody.velocity = new Vector3(moveVector.x, characterRigidBody.velocity.y, characterRigidBody.velocity.z);
+                //Debug.Log(characterRigidBody.velocity);
 
             }
             
         }
         if(dir_mov == Mov_Dir.right){
             if(!checkWallOnFront){
-                Vector3 moveVector = transform.TransformDirection(direction)*speed;
+                Vector3 moveVector = direction*speed;
                 characterRigidBody.velocity = new Vector3(moveVector.x, characterRigidBody.velocity.y, characterRigidBody.velocity.z);
+                //Debug.Log(characterRigidBody.velocity);
                 
 
             }
             
         }
+        
 
+        //Movimiento con Fisicas no tan correctas - Menos pesado
         /*if(Input.GetKey("right")  && dir_mov == Mov_Dir.right){
             if(!checkWallOnFront){
-                Vector3 moveVector = transform.TransformDirection(direction)*speed;
-                //characterRigidBody.velocity = new Vector3(moveVector.x, characterRigidBody.velocity.y, characterRigidBody.velocity.z);
+                
                 characterRigidBody.Move(new Vector3(characterRigidBody.transform.position.x + (speed*Time.fixedDeltaTime),characterRigidBody.transform.position.y,
                 characterRigidBody.transform.position.z),Quaternion.identity);
 
             }
             
+        }
+
+        
+        if(Input.GetKey("left")  && dir_mov == Mov_Dir.left){
+            if(!checkWallOnFront){
+                characterRigidBody.Move(new Vector3(characterRigidBody.transform.position.x + (-speed*Time.fixedDeltaTime),characterRigidBody.transform.position.y,
+                characterRigidBody.transform.position.z),Quaternion.identity);
+
+            }
+
+            
         }*/
-           
-           
+
+
+
+        movement = new Vector3(0.0f, 0.0f ,0.0f);
+        
+        
 
         
     }
