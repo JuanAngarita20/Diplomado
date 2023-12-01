8b541c65d72ca216fcffbd6e9eac96106cb89a3f
JuanAngarita20 <80299132+JuanAngarita20@users.noreply.github.com>
2023-12-01 02:21:53 -0500

    Avances 1

---
+++ Prototipo Tuki/Assets/Scripts/PlayerFalling.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFalling : MonoBehaviour
+{
+    [SerializeField] private Rigidbody rg;
+    [SerializeField] private Animator animator = null;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log(rg.velocity.y );
+        if(rg.velocity.y < -2f){
+            Debug.Log("Cayendo!!");
+            animator.SetBool("falling",true);
+
+        }
+        else{
+            animator.SetBool("falling",false);
+        }
+
+    }
+
+      private void OnCollisionEnter(Collision collision){
+
+
+        if(collision.gameObject.tag.Equals("Floor")){
+            animator.SetBool("falling",false);
+        }
+
+        
+    }
+
+}
